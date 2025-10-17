a9fad78f61735c412a801c9402002365e922c54c
enszngn <eneszengin2006@outlook.com>
2025-10-17 15:18:20 +0300

    labler zorlasti az

---
+++ Labguides/LabGuide_7_q4.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    int num, basamak, i, sum = 0, mult = 1, p;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    i = num;
+    p = num;
+
+    while(i > 0){
+        sum += i % 10;
+        i /= 10;
+    }
+    while(p > 0){
+        mult *= p % 10;
+        p /= 10;
+    }
+    printf("Given num: %d, Sum: %d, Product: %d\n", num, sum, mult);
+
+    if(sum == mult)
+        printf("%d is a spy number :)\n", num);
+    else
+        printf("%d is not a spy number :(\n", num);
+    
+    return 0;
+}
