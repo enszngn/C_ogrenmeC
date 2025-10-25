6cfba6ab09a262c665fe4adea7052d0365bd41d0
enszngn <eneszengin2006@outlook.com>
2025-10-25 19:44:41 +0300

    lab9 zor gibi

--- Labguides/LabGuide9.cpp
+++ Labguides/LabGuide9.cpp
@@ -3,27 +3,44 @@
 #include <time.h>
 
 int main(){
-    int randnum, choice;
+    int randnum, choice = 0, i, p, j;
+    char bos = ' ';
     srand(time(NULL));
     
-    while(choice != 3)
+    while(choice != 3){
         randnum = rand() % 7 + 3;
     
-    printf("           MENU\n");
-    printf("-----------------------\n");
-    printf("1 - Right Triangle\n");
-    printf("2 - Isosceles Triangle\n");
-    printf("3 - Exit\n");
-    printf("Enter your choice: ");
-    scanf(" %d", &choice);
+        printf("           MENU\n");
+        printf("--------------------------\n");
+        printf("1 - Right Triangle\n");
+        printf("2 - Isosceles Triangle\n");
+        printf("3 - Exit\n");
+        printf("Enter your choice: ");
+        scanf(" %d", &choice);
 
-    switch(choice){
-        case 1:
-        //code for right triangle
-            break;
-        case 2:
-        //code for isosceles triangle
-            
+        switch(choice){
+            case 1:
+            printf("Random number is %d\n", randnum);
+            for(i = 1; i <= randnum; i++){
+                for(p = 1; p <= randnum - i; p++)
+                    printf("%c", bos);
+                for(p = 1; p <= i; p++)
+                    printf("%d", p);
+                printf("\n");
+            }
+                break;
+            case 2:
+            printf("Random number is %d\n", randnum);
+            for(i = 1; i <= randnum; i++){
+                for(p = 1; p <= randnum - i; p++)
+                    printf("%c", bos);
+                for(p = 1; p <= i; p++)
+                    printf("%d", p);
+                for(p = i - 1; p >= 1; p--)
+                    printf("%d",p);
+                printf("\n");
+            }
+        }
     }
     
     return 0;
