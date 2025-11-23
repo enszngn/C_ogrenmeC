9bceb7c4baba64a58d8c97ee2f5076171b31b1cc
enszngn <eneszengin2006@outlook.com>
2025-11-23 19:57:08 +0300

    duzeltilmis aq2.cpp

--- Labguides/LabGuide12/aq2.cpp
+++ Labguides/LabGuide12/aq2.cpp
@@ -9,25 +9,20 @@ int main(){
 	else{
 		FILE *outp;
 		outp = fopen("compressed.txt", "w");
-		int status, numOfChar = 2;
+		int status, numOfChar = 0;
 		char ch;
 		status = fscanf(inp, "%c", &ch);
 		while(status != EOF){
 			char temp = ch;
-			status = fscanf(inp, "%c", &ch);
-			while(temp == ch){
+			while(status != EOF && temp == ch){
 				numOfChar++;
 				status = fscanf(inp, "%c", &ch);
 			}
-			if(numOfChar > 2){
+			if(numOfChar > 1)
 				fprintf(outp, "%c%d", temp, numOfChar);
-			}
 			else
 				fprintf(outp, "%c", temp);
-			numOfChar = 1;
-			status = fscanf(inp, "%c", &ch);
-			if(ch == '\n')
-				fprintf(outp, "\n");
+			numOfChar = 0;
 		}
 		fclose(inp);
 		fclose(outp);
