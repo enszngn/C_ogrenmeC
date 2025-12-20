583158e4167cd1a4ee70ea561cf802ac3a2107da
enszngn <eneszengin2006@outlook.com>
2025-12-20 20:32:21 +0300

    l18 aq1.cpp

--- Labguides/LabGuide18/aq1.cpp
+++ Labguides/LabGuide18/aq1.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int findTotalVote(int votes[][20], int columnSize, char givenCity, int partyVotes[]);
+int findTotalVote(int votes[][20], char cities[], int rows, char givenCity, int partyVotesInGivenCity[5]);
 
-double findPercentage(int votesParty, int votesTotal);
+double findPercentage(int votes, int totalVotes);
 
 int main(){
 
@@ -11,22 +11,22 @@ int main(){
 	if(inp == NULL)
 		printf("ERROR");
 	else{
-		int votes[3][20];
-		int partyVotesAnkara[5];
-		int partyVotesIstanbul[5];
-		int status, i = 0;
-		status = fscanf(inp, "%d %d %c", &votes[1][i], &votes[2][i], &votes[3][i]);
-		while(status != EOF){
-			i++;
-			status = fscanf(inp, "%d %d %c", &votes[1][i], &votes[2][i], &votes[3][i]);
+		int votes[2][20];
+		char cities[20];
+		int partyVotesAnkara[5] = {0}, totalAnkara = 0;
+		int partyVotesIstanbul[5] = {0}, totalIstanbul = 0;
+		int rows = 0;
+		while(rows < 20 && fscanf(inp, "%d %d %c", &votes[0][rows], &votes[1][rows], &cities[rows]) == 3){
+			rows++;
 		}
-		int totalAnkara = findTotalVote(votes, 3, 'A', partyVotesAnkara);
-		int totalIstanbul = findTotalVote(votes, 3, 'I', partyVotesIstanbul);
-		printf("City Code       Ankara       Istanbul \n");
-		printf("*********     **********    **********\n");
-		printf("  Total          %4d           %4d   \n", totalAnkara, totalIstanbul);
-		for(int i = 0; i < 5; i++){
-			printf("    %d         %d  %4.1f     %d   %4.1f",
+		totalAnkara = findTotalVote(votes, cities, rows, 'A', partyVotesAnkara);
+		totalIstanbul =  findTotalVote(votes, cities, rows, 'I', partyVotesIstanbul);
+		printf("City Code       Ankara        Istanbul\n");
+		printf("*********     **********     **********\n");
+		printf("  Total        	 %d           %d   \n", totalAnkara, totalIstanbul);
+		printf("*********     **********     **********\n");
+		for(int p = 0; p < 5; p++){
+			printf("    %-5d     %3d  %4.1f%%     %d  %4.1f\n", p + 1, partyVotesAnkara[p], findPercentage(partyVotesAnkara[p], totalAnkara), partyVotesIstanbul[p], findPercentage(partyVotesIstanbul[p], totalIstanbul));
 		}
 		fclose(inp);
 	}
@@ -34,17 +34,17 @@ int main(){
 	return 0;
 }
 
-int findTotalVote(int votes[][20], int columnSize, char givenCity, int partyVotes[]){
-	int size = 0;
-	for(int i = 0; i < 20; i++){
-		if(votes[3][i] == givenCity){
-			size += votes[2][i];
-			partyVotes[votes[0][i] - 1] += votes[2][i];
+int findTotalVote(int votes[][20], char cities[], int rows, char givenCity, int partyVotesInGivenCity[5]){
+	int totalVote = 0;
+	for(int i = 0; i < rows; i++){
+		if(cities[i] == givenCity){
+			partyVotesInGivenCity[votes[0][i] - 1] += votes[1][i];
+			totalVote += votes[1][i];
 		}
 	}
-	return size;
+	return totalVote;
 }
 
-double findPercentage(int votesParty, int votesTotal){
-	return 1.0 * votesParty / votesTotal * 100; 
+double findPercentage(int votes, int totalVotes){
+	return 100.0 * votes / totalVotes;
 }
